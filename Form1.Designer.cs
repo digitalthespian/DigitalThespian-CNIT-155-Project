d79d7b3c8780d913b8ac40543b7b16fecfc1b18b
digitalthespian <kridge@purdue.edu>
2018-04-15 21:12:39 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace DigitalThespian_CNIT_155_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGenreFilter = new System.Windows.Forms.TextBox();
+            this.txtRatingFilter = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.radTitle = new System.Windows.Forms.RadioButton();
+            this.radGenre = new System.Windows.Forms.RadioButton();
+            this.radRating = new System.Windows.Forms.RadioButton();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.txtRating = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 16;
+            this.lstOutput.Location = new System.Drawing.Point(12, 86);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(345, 196);
+            this.lstOutput.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(431, 165);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 4;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(15, 83);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(133, 23);
+            this.btnFilter.TabIndex = 5;
+            this.btnFilter.Text = "Add Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(431, 252);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(431, 194);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 7;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(15, 112);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(133, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save Filtered List";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtGenreFilter);
+            this.groupBox1.Controls.Add(this.txtRatingFilter);
+            this.groupBox1.Controls.Add(this.btnFilter);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Location = new System.Drawing.Point(358, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(159, 147);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filters";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Rating";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Genre";
+            // 
+            // txtGenreFilter
+            // 
+            this.txtGenreFilter.Location = new System.Drawing.Point(55, 19);
+            this.txtGenreFilter.Name = "txtGenreFilter";
+            this.txtGenreFilter.Size = new System.Drawing.Size(93, 22);
+            this.txtGenreFilter.TabIndex = 10;
+            // 
+            // txtRatingFilter
+            // 
+            this.txtRatingFilter.Location = new System.Drawing.Point(55, 46);
+            this.txtRatingFilter.Name = "txtRatingFilter";
+            this.txtRatingFilter.Size = new System.Drawing.Size(93, 22);
+            this.txtRatingFilter.TabIndex = 9;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(431, 223);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(12, 31);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(116, 22);
+            this.txtTitle.TabIndex = 13;
+            // 
+            // radTitle
+            // 
+            this.radTitle.AutoSize = true;
+            this.radTitle.Checked = true;
+            this.radTitle.Location = new System.Drawing.Point(72, 59);
+            this.radTitle.Name = "radTitle";
+            this.radTitle.Size = new System.Drawing.Size(56, 21);
+            this.radTitle.TabIndex = 18;
+            this.radTitle.TabStop = true;
+            this.radTitle.Text = "Title";
+            this.radTitle.UseVisualStyleBackColor = true;
+            this.radTitle.CheckedChanged += new System.EventHandler(this.radTitle_CheckedChanged);
+            // 
+            // radGenre
+            // 
+            this.radGenre.AutoSize = true;
+            this.radGenre.Location = new System.Drawing.Point(198, 59);
+            this.radGenre.Name = "radGenre";
+            this.radGenre.Size = new System.Drawing.Size(69, 21);
+            this.radGenre.TabIndex = 19;
+            this.radGenre.Text = "Genre";
+            this.radGenre.UseVisualStyleBackColor = true;
+            this.radGenre.CheckedChanged += new System.EventHandler(this.radGenre_CheckedChanged);
+            // 
+            // radRating
+            // 
+            this.radRating.AutoSize = true;
+            this.radRating.Location = new System.Drawing.Point(282, 59);
+            this.radRating.Name = "radRating";
+            this.radRating.Size = new System.Drawing.Size(70, 21);
+            this.radRating.TabIndex = 20;
+            this.radRating.Text = "Rating";
+            this.radRating.UseVisualStyleBackColor = true;
+            this.radRating.CheckedChanged += new System.EventHandler(this.radRating_CheckedChanged);
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Location = new System.Drawing.Point(150, 31);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(117, 22);
+            this.txtGenre.TabIndex = 21;
+            // 
+            // txtRating
+            // 
+            this.txtRating.Location = new System.Drawing.Point(282, 31);
+            this.txtRating.Name = "txtRating";
+            this.txtRating.Size = new System.Drawing.Size(70, 22);
+            this.txtRating.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 291);
+            this.Controls.Add(this.txtRating);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.radRating);
+            this.Controls.Add(this.radGenre);
+            this.Controls.Add(this.radTitle);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lstOutput);
+            this.Name = "Form1";
+            this.Text = "Movie Database";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.RadioButton radTitle;
+        private System.Windows.Forms.RadioButton radGenre;
+        private System.Windows.Forms.RadioButton radRating;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.TextBox txtRating;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGenreFilter;
+        private System.Windows.Forms.TextBox txtRatingFilter;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DigitalThespian_CNIT_155_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ////////////////////////////////////////////
+        //Variable Declaration
+        ////////////////////////////////////////////
+        private int arraysize;
+        private string[] mTitle = new string[50];//parallel arrays (Rubric Requirement)
+        private string[] mGenre = new string[50];
+        private string[] mRating = new string[50];
+        string[] mFilter = new string[50];//these exist for the "saving filtered list" part
+        int mFilterIndex = 0;             //
+        private int mIndex = 0;
+        private int ctr;
+
+        ////////////////////////////////////////////
+        //Helper Methods    (Rubric Requirement)
+        ////////////////////////////////////////////
+
+        private bool validate()
+        {
+            if (txtTitle.Text == "" || txtGenre.Text == "" || txtRating.Text == "")
+            {
+                displaymessage("Please enter all movie data.", "Incomplete entry");
+                txtTitle.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void displaymessage(string message, string header)
+        {
+            MessageBox.Show(message, header, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void arraysort() //sorts list by whichever radio button is selected 
+        {
+            string[] sortingarray = new string[50]; //this exists to allow me to have one loop regardless 
+            string chk;                             //of which array is the "key", so to speak
+            int ctr;
+            int sort;
+            if (radTitle.Checked == true)//sort parameters based on radio buttons
+            {
+                Array.Copy(mTitle, sortingarray, 50); //this copies the "key" array over to the var sortingarray
+            }
+            else if (radGenre.Checked == true)
+            {
+                Array.Copy(mGenre, sortingarray, 50);
+            }
+            else
+            {
+                Array.Copy(mRating, sortingarray, 50);
+            }
+
+            for (sort = 0; sort < mIndex - 1; sort++)
+            {
+                for (ctr = 0; ctr < mIndex - 1; ctr++)
+                {
+                    chk = sortingarray[ctr];
+                    if (chk.CompareTo(sortingarray[ctr + 1]) > 0)//this is literally just the bubble sort code
+                    {                                            //from lab, but changing all 4 arrays at once
+                        string temp = mTitle[ctr + 1];
+                        chk = mTitle[ctr];
+                        mTitle[ctr + 1] = chk;
+                        mTitle[ctr] = temp;
+
+                        temp = mGenre[ctr + 1];
+                        chk = mGenre[ctr];
+                        mGenre[ctr + 1] = chk;
+                        mGenre[ctr] = temp;
+
+                        temp = mRating[ctr + 1];
+                        chk = mRating[ctr];
+                        mRating[ctr + 1] = chk;
+                        mRating[ctr] = temp;
+
+                        temp = sortingarray[ctr + 1];
+                        chk = sortingarray[ctr];
+                        sortingarray[ctr + 1] = chk;
+                        sortingarray[ctr] = temp;
+                    }
+                }
+            }
+        }
+
+        ////////////////////////////////////////////
+        //Main Body
+        ////////////////////////////////////////////
+
+        private void Form1_Load(object sender, EventArgs e)//checks if a filtered list has been made, loads it if yes
+        {
+            if (File.Exists("MoviesFiltered.txt") == true)
+            {
+                StreamReader SR = new StreamReader("MoviesFiltered.txt");
+                arraysize = File.ReadLines("MoviesFiltered.txt").Count();
+                if (arraysize > 50)
+                {
+                    displaymessage("The file is too large.", "Error");//this is in here because i was worried about using  
+                    return;                                           //a list, and you HAVE to give arrays a length, so i said 50
+                }
+
+                string[] storage; //dummy array to hold values before splitting them into their proper arrays
+
+                string line = SR.ReadLine();
+                
+                while (line != null)//SR loop
+                {
+                    storage = line.Split('\t');
+                    mTitle[mIndex] = storage[0];
+                    mGenre[mIndex] = storage[1];
+                    mRating[mIndex] = storage[2];
+                    line = SR.ReadLine();
+                    mIndex++;
+                }
+
+                for (ctr = 0; ctr < mIndex; ctr++) //prints to the listbox
+                {
+                    lstOutput.Items.Add(mTitle[ctr].PadRight(20) + mGenre[ctr].PadRight(20) + mRating[ctr]);
+                }
+            }
+            else if (File.Exists("Movies.txt") == true)//same deal, but if there isnt a filtered list
+            {
+                StreamReader SR = new StreamReader("Movies.txt");
+                arraysize = File.ReadLines("Movies.txt").Count();
+                if (arraysize > 50)
+                {
+                    displaymessage("The file is too large.", "Error");
+                    return;
+                }
+
+                string[] storage; //dummy array to hold values before splitting them into their proper arrays
+
+                string line = SR.ReadLine();
+
+                while (line != null)
+                {
+                    storage = line.Split('\t');
+                    mTitle[mIndex] = storage[0];
+                    mGenre[mIndex] = storage[1];
+                    mRating[mIndex] = storage[2];
+                    line = SR.ReadLine();
+                    mIndex++;
+                }
+
+                for (ctr = 0; ctr < mIndex; ctr++) //prints to the listbox
+                {
+                    lstOutput.Items.Add(mTitle[ctr].PadRight(20) + mGenre[ctr].PadRight(20) + mRating[ctr]);
+                }
+            }
+            else
+            {
+                displaymessage("No existing file.", "Notification");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //i dont need this but i'm kind of afraid to try and delete it
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            if (txtGenreFilter.Text == "" && txtRatingFilter.Text == "")//if there is nothing, it clears back to 
+            {                                                           //the original list and clears mFilter
+                lstOutput.Items.Clear();
+                Array.Clear(mFilter, 0, 50);
+                for (ctr = 0; ctr < mIndex; ctr++)
+                {
+                    lstOutput.Items.Add(mTitle[ctr].PadRight(20) + mGenre[ctr].PadRight(20) + mRating[ctr]);
+                }
+                return;
+            }
+
+
+            lstOutput.Items.Clear();
+            for (ctr = 0; ctr < mIndex; ctr++)//this prints all movies with the entered genre/rating,
+            {                                 //and adds them to mFilter to be saved later
+                if (txtGenreFilter.Text.ToUpper() == mGenre[ctr].ToUpper() || txtRatingFilter.Text.ToUpper() == mRating[ctr].ToUpper())
+                {
+                    lstOutput.Items.Add(mTitle[ctr].PadRight(20) + mGenre[ctr].PadRight(20) + mRating[ctr]);
+                    mFilter[mFilterIndex] = mTitle[ctr] + '\t' + mGenre[ctr] + '\t' + mRating[ctr];//mFilter is in SW form already here
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (mIndex == 0)
+            {
+                displaymessage("No data entered.", "Error");
+                txtTitle.Focus();
+                return;
+            }
+
+            if (mFilter[0] != null)//if mFilter exists, it saves it to the filtered text file, otherwise
+            {                      //it saves the movies list back over the old movies file (or makes one)
+                StreamWriter SW = new StreamWriter("MoviesFiltered.txt");
+
+                for (ctr = 0; ctr < mIndex; ctr++)
+                {
+                    SW.WriteLine(mFilter[ctr]);
+                }
+
+                SW.Close();
+            }
+            else
+            {
+                StreamWriter SW = new StreamWriter("Movies.txt");
+
+                for (ctr = 0; ctr < mIndex; ctr++)
+                {
+                    SW.WriteLine(mTitle[ctr] + '\t' + mGenre[ctr] + '\t' + mRating[ctr]);
+                }
+
+                SW.Close();
+            }
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            if (validate() == false)
+            {
+                return;
+            }
+
+            mTitle[mIndex] = txtTitle.Text;//i tried doing this with combo boxes and it was NOT having it
+            mGenre[mIndex] = txtGenre.Text;//so if you guys can figure that out be my guest
+            mRating[mIndex] = txtRating.Text;
+            mIndex++;
+
+            arraysort(); //sorts new movie into list
+
+            txtTitle.Clear(); txtGenre.Clear(); txtRating.Clear();
+            lstOutput.Items.Clear();
+            for (ctr = 0; ctr < mIndex; ctr++) //reprints the listbox with the new list
+            {
+                lstOutput.Items.Add(mTitle[ctr].PadRight(20) + mGenre[ctr].PadRight(20) + mRating[ctr]);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstOutput.SelectedIndex == -1)
+            {
+                displaymessage("You must select a movie to remove it.", "Invalid selection");
+                return;
+            }
+
+            if (mIndex == 0)
+            {
+                displaymessage("No data entered.", "Error");
+                txtTitle.Focus();
+                return;
+            }
+
+            int index;
+            index = lstOutput.SelectedIndex;//this is the removal loop i wrote in lab, i'm pretty sure almost
+                                            //everyone did it this way
+            for (ctr = index; ctr < mIndex - 1; ctr++)
+            {
+                mTitle[ctr] = mTitle[ctr + 1];
+                mGenre[ctr] = mGenre[ctr + 1];
+                mRating[ctr] = mRating[ctr + 1];
+            }
+            mIndex--;
+            lstOutput.Items.Clear();
+            for (ctr = 0; ctr < mIndex; ctr++)
+            {
+                lstOutput.Items.Add(mTitle[ctr].PadRight(20) + mGenre[ctr].PadRight(20) + mRating[ctr]);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();//please dont make me explain this
+        }
+
+        private void radTitle_CheckedChanged(object sender, EventArgs e)//these make it so when the user changes 
+        {                                                               //sort criteria, it re-sorts immediately
+            arraysort();
+            lstOutput.Items.Clear();
+            for (ctr = 0; ctr < mIndex; ctr++)
+            {
+                lstOutput.Items.Add(mTitle[ctr].PadRight(20) + mGenre[ctr].PadRight(20) + mRating[ctr]);
+            }
+        }
+
+        private void radGenre_CheckedChanged(object sender, EventArgs e)
+        {
+            arraysort();
+            lstOutput.Items.Clear();
+            for (ctr = 0; ctr < mIndex; ctr++)
+            {
+                lstOutput.Items.Add(mTitle[ctr].PadRight(20) + mGenre[ctr].PadRight(20) + mRating[ctr]);
+            }
+        }
+
+        private void radRating_CheckedChanged(object sender, EventArgs e)
+        {
+            arraysort();
+            lstOutput.Items.Clear();
+            for (ctr = 0; ctr < mIndex; ctr++)
+            {
+                lstOutput.Items.Add(mTitle[ctr].PadRight(20) + mGenre[ctr].PadRight(20) + mRating[ctr]);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)//search is based on which radio button is pressed
+        {                                                       //yay multifunctionality
+            if (radTitle.Checked == true)
+            {
+                for (ctr = 0; ctr < mIndex; ctr++)//this just checks the list against what is in txtTitle
+                {                                 //and prints all of the matches (case-insensitive)
+                    if (txtTitle.Text.ToUpper() == mTitle[ctr].ToUpper())
+                    {
+                        lstOutput.SelectedIndex = ctr;
+                    }
+                }
+            }
+            else if (radGenre.Checked == true)
+            {
+                for (ctr = 0; ctr < mIndex; ctr++)
+                {
+                    if (txtGenre.Text.ToUpper() == mGenre[ctr].ToUpper())
+                    {
+                        lstOutput.SelectedIndex = ctr;
+                    }
+                }
+            }
+            else
+            {
+                for (ctr = 0; ctr < mIndex; ctr++)
+                {
+                    if (txtRating.Text.ToUpper() == mRating[ctr].ToUpper())
+                    {
+                        lstOutput.SelectedIndex = ctr;
+                    }
+                }
+            }
+        }           //ending comments: I've been coding for like, three hours, so I haven't gone over this 
+    }               //quite as thoroughly as i'd like, so it very well may have runtime errors
+}
